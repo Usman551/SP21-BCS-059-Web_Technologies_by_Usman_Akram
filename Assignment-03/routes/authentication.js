e78cdab8a8f09d9c9ca88631f785882c73f979d0
Muhammad Usman <148494076+Usman551@users.noreply.github.com>
2024-05-28 15:17:19 -0700

    Add files via upload

---
+++ Assignment-03/routes/authentication.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+var User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+const config = require("config");
+const bcrypt = require("bcryptjs");
+router.post("/", async function (req, res, next) {
+  let user = await User.findOne({
+    email: req.body.email,
+  });
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) {
+    return res.status(400).send("Invalid Password");
+  }
+  const token = jwt.sign(
+    {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+    config.get("jwtPrivateKey")
+  );
+
+  return res.send(token);
+});
+
+module.exports = router;
